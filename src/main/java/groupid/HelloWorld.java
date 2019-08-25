62198a6ec0a2a2fdb84120196073ce5408bb6b23
aradess <53818808+aradess@users.noreply.github.com>
2019-08-25 21:24:32 +0300

    usage TestList

--- src/main/java/groupid/HelloWorld.java
+++ src/main/java/groupid/HelloWorld.java
@@ -5,18 +5,17 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @ManagedBean
 @SessionScoped
 public class HelloWorld {
+
     private final static List<String> ENTITIES = Arrays.asList(new String[]{"1", "2", "3", "4", "5"});
+
+    private final String[] array = new String[]{"1", "2", "3", "4", "5"};
+    private List<TestList> listTextArea;
 
     @PostConstruct
     public void init() {
@@ -26,12 +25,15 @@ public class HelloWorld {
         values.put("2", "2");
         values.put("3", "3");
         values.put("4", "4");
+        //----------
+        listTextArea = new ArrayList<>();
+
     }
 
     private String selectedType = "";
     private Set<String> availableFields;
     private Set<String> selectedFields;
-    private Map<String, String> values = new HashMap<String, String>();
+    private Map<String, String> values = new HashMap<>();
 
     public Set<String> getAvailableFields() {
         return availableFields;
@@ -57,11 +59,8 @@ public class HelloWorld {
         this.values = values;
     }
 
-    public void change(ValueChangeEvent type) {
-        if (type == null || type.getNewValue() == null || type.getNewValue() == "") {
-            return;
-        }
-        Optional<String> f = availableFields.stream().filter(s -> s.equals(type.getNewValue())).findFirst();
+    public void change(String type) {
+        Optional<String> f = availableFields.stream().filter(s -> s.equals(type)).findFirst();
         selectedFields.add(f.get());
         availableFields.remove(f.get());
         values.put(f.get(), null);
@@ -74,12 +73,34 @@ public class HelloWorld {
         values.put(f.get(), null);
     }
 
+    public void change2() {
+        TestList ttt = new TestList();
+        ttt.setNomerRow("1");
+        ttt.setArea(selectedType);
+        listTextArea.add(ttt);
+    }
+
+    public void onDelete2(TestList test) {
+        if (!listTextArea.isEmpty()) {
+            listTextArea.remove(test);
+            System.out.println(listTextArea.size());
+        }
+    }
+
     public String getSelectedType() {
         return selectedType;
     }
 
     public void setSelectedType(String selectedType) {
         this.selectedType = selectedType;
+    }
+
+    public List<TestList> getListTextArea() {
+        return listTextArea;
+    }
+
+    public void setListTextArea(List<TestList> listTextArea) {
+        this.listTextArea = listTextArea;
     }
 }
 
---
+++ src/main/java/groupid/TestList.java
@@ -0,0 +1,22 @@
+package groupid;
+
+public class TestList {
+    private String nomerRow;
+    private String area;
+
+    public String getNomerRow() {
+        return nomerRow;
+    }
+
+    public void setNomerRow(String nomerRow) {
+        this.nomerRow = nomerRow;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
